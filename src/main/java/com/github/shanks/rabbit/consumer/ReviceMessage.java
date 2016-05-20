13edc384457925874cc86d30f53da2decfb6b038
shanks-devil <wangliang.devil@gmail.com>
2016-05-20 09:43:49 +0800

    Consumer(properties): modify configuration
    Modify configuration

--- src/main/java/com/github/shanks/rabbit/consumer/ReviceMessage.java
+++ src/main/java/com/github/shanks/rabbit/consumer/ReviceMessage.java
@@ -2,15 +2,15 @@ package com.github.shanks.rabbit.consumer;
 
 import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
 
 import com.github.shanks.rabbit.msg.MessageModel;
 import com.rabbitmq.client.Channel;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RabbitListener
+@Component
 @Slf4j
 public class ReviceMessage implements ChannelAwareMessageListener {
 
@@ -19,10 +19,10 @@ public class ReviceMessage implements ChannelAwareMessageListener {
 		try {
 			MessageModel messageModel = SerializationUtils.deserialize(message.getBody());
 			System.out.println(messageModel);
-			channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 		} catch (Exception e) {
 			log.error(e.getMessage(),e);
-			channel.basicNack(message.getMessageProperties().getDeliveryTag(), true, true);
+			channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
 		}
 	}
 
